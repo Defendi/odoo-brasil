b7f4bb74e534fcce514198bf9ffdf00dfe1aed31
defendi <alexandre_defendi@hotmail.com>
2018-09-25 15:12:49 -0300

    NFCe

--- br_nfce/models/__init__.py
+++ br_nfce/models/__init__.py
@@ -2,4 +2,5 @@
 # © 2016 Danimar Ribeiro <danimaribeiro@gmail.com>, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from . import res_company
 from . import invoice_eletronic
