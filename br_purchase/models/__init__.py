ab146bf74ee6de0c84f73002a2f83e5781d0dee8
defendi <alexandre_defendi@hotmail.com>
2019-02-06 18:24:26 -0200

    Vencimento da Solicitação para compra

--- br_purchase/models/__init__.py
+++ br_purchase/models/__init__.py
@@ -2,6 +2,6 @@
 # © 2016 Danimar Ribeiro, Trustcode
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-
+from . import res_config_settings
 from . import purchase
 from . import account_invoice
