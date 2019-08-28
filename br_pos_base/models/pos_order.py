1a1ed3278b890ac7ab3c58b1eaa4fba53d08cb7a
defendi <alexandre_defendi@hotmail.com>
2019-08-28 16:01:25 -0300

    Ajuste no auto envia nfce do pos

--- br_pos_base/models/pos_order.py
+++ br_pos_base/models/pos_order.py
@@ -6,3 +6,9 @@ class PosOrder(models.Model):
     _inherit = 'pos.order'
 
     cnpj_cpf = fields.Char('CNPJ/CPF', size=18, copy=False, default='Não Indicado')
+    
+    def _action_create_invoice_line(self, line=False, invoice_id=False):
+        res = super(PosOrder,self)._action_create_invoice_line(line=line, invoice_id=invoice_id)
+        if len(res.product_id) > 0:
+            res.write({'fiscal_classification_id': res.product_id.fiscal_classification_id.id or False})
+        return res
