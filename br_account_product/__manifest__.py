f9a136b38bfc2520b7d40d95ddc5ba36175d6c41
defendi <alexandre_defendi@hotmail.com>
2019-11-08 11:33:48 -0300

    Módulo br_account_product

---
+++ br_account_product/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{  
+    'name': 'Brazilian Localization Account Product',
+    'description': """Brazilian Localization Account Product""",
+    'version': '11.0.1.0.0',
+    'category': 'account',
+    'author': 'DocSafe',
+    'license': 'AGPL-3',
+    'website': 'http://www.docsafe.com.br',
+    'contributors': [
+        'Alexandre Defendi <alexandredefendi4@gmail.com>'
+    ],
+    'depends': [
+        'br_base', 'product', 'account',
+    ],
+    'data': [
+        'views/product_view.xml',
+    ],
+}
