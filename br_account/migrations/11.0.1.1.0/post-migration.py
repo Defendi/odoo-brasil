a7c0b05a4e32c2de21107a31eaf7f847bbed83ee
Defendi <alexandre_defendi@hotmail.com>
2020-05-14 16:25:21 -0300

    Retirado o migrate

--- br_account/migrations/11.0.1.1.0/post-migration.py
+++ br_account/migrations/11.0.1.1.0/post-migration.py
@@ -3,11 +3,11 @@ def migrate(cr, version):
     if not version:
         return
 
-    cr.execute("SELECT id, name, nat_operacao FROM account_fiscal_position;")
-    positions = cr.fetchall()
-    _logger.info('Getting started ...')
-    for position in enumerate(positions):
-        if len(position[2]) <= 1:
-            sql = "UPDATE account_fiscal_position SET nat_operacao = '%s'" % position[1]
-            cr.execute(sql)
+#     cr.execute("SELECT id, name, nat_operacao FROM account_fiscal_position;")
+#     positions = cr.fetchall()
+#     _logger.info('Getting started ...')
+#     for position in enumerate(positions):
+#         if len(position[2]) <= 1:
+#             sql = "UPDATE account_fiscal_position SET nat_operacao = '%s'" % position[1]
+#             cr.execute(sql)
 
