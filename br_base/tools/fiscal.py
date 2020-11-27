31cd51a02f1cec4534a8fd1bb6429362cdaa8dd7
Defendi <alexandre_defendi@hotmail.com>
2020-11-27 16:59:01 -0300

    funcao de calculo do digito do cnpj

--- br_base/tools/fiscal.py
+++ br_base/tools/fiscal.py
@@ -490,3 +490,17 @@ def format_cpf_cnpj(numero,limpa=False):
         elif x == 14:
             res = "%s.%s.%s/%s-%s" % (valor[:2],valor[2:5],valor[5:8],valor[8:12],valor[12:14])
     return res
+
+def Calcula_digito(cnpj):
+    ncnpj = list(map(int, cnpj))
+    novo = ncnpj[:12]
+    prod = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+    while len(novo) < 14:
+        r = sum([x * y for (x, y) in zip(novo, prod)]) % 11
+        if r > 1:
+            f = 11 - r
+        else:
+            f = 0
+        novo.append(f)
+        prod.insert(0, 6)
+    return novo
